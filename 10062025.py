665e03360e40cec4caf1cee734f41b348e80d7d3
Alok Sharma <sharma.prince2007@gmail.com>
2025-06-11 09:47:20 +0530

    New file named '11062025' is added

---
+++ 10062025.py
@@ -0,0 +1,41 @@
+# # a=5
+# # b=6
+# # c=a+b
+# # print(c)
+
+# # Sum of 3 No.
+# a = int(input("Enter first Number: "))
+# b = int(input("enter second Number: "))
+# c = int(input("Enter the third number: "))
+# sum = a+b+c
+# print(sum)
+
+# If and else stetements
+"""age = int(input("input your age please : "))
+if age >=  18:
+    print("Congratz! You are eligible to vote.")
+else:
+    print("Sorry! You can't vote.")"""
+
+"""def findsum(a,b):
+    result = a + b
+    return result
+x=5
+y=7
+z=findsum(x,y)
+print(z)"""
+
+"""def sum_num(num1,*num):
+    result=num1
+    for i in num:
+        result +=i
+    return result
+r = sum_num(10,20,30)
+print(f"The sum of number is: {r}")"""
+
+"""def generate_report(name,marks,subject="Python"):
+    average =  sum(marks)/len(marks)
+    grade = "A" if average >= 90 else "B" if average >=75 else "C"
+    print(f"{name}'s {subject} Report \nMarks : {marks} \nAverage : {average} \nGrade : {grade}\n")
+generate_report("Aman Kumar Singh",[85,92,78])"""
+
