7642f62ded4aa47c01fe1e413d0f18ce0590b49c
Alok Sharma <sharma.prince2007@gmail.com>
2025-06-11 15:49:22 +0530

    remove countdown script from extra.py

--- extra.py
+++
@@ -1,6 +0,0 @@
-# This script counts down from 10 to 1, printing the time left each second.
-"""import time as t
-for i in range(10,0,-1):
-    print("Time Left:",i,"seconds")
-    t.sleep(1)  # Sleep for 1 second
-print("Time's up!")"""
