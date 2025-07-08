5c2cf0e49aaf1392884157b89f9eec3b9c6ff2c6
Alok Sharma <sharma.prince2007@gmail.com>
2025-07-08 12:05:07 +0530

    new files and content added

---
+++ 18062025.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+# import numpy as np
+# import pandas as pd
+# df1 = pd.DataFrame({'A':['A0','A1','A3'],'B': ['B0','B1','B3']},index=[0,1,2])
+# df2 = pd.DataFrame({'A':['A5','A6','A7'],'B': ['B5','B6','B7']},index=[3,4,5])
+# df3 = pd.DataFrame({'A':['A9','A10','A11'],'B': ['B9','B10','B11']},index=[6,7,8])
+# print(df1)
+# print(df2)
+# print(df3)
+# print(pd.concat([df1,df2,df3],axis=1))
+
+
+# import pandas as pd
+# left = pd.DataFrame({'A':['A0','A1','A3'],'B': ['B0','B1','B3']},index=['K0','K1','K2'])
+# right = pd.DataFrame({'A':['A5','A6','A7'],'B': ['B5','B6','B7']},index=['K0','K2','K3'])
+# print(pd.join)
+
+
---
+++ 19062025.py
@@ -0,0 +1,12 @@
+# import matplotlib.pyplot as plt
+# import numpy as np
+# x_point = np.array([5,6,4,5,7,6])
+# y_point = np.array([15,20,35,54,76,86])
+# z_point = np.array([1,1.5,2,2.5,3,3.5])
+# plt.plot(x_point,y_point,z_point)
+# plt.show()
+
+
+# from random import sample
+# import numpy as np
+# data = [np.random.normal(0,100)]
